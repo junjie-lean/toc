5d4b2c7017ca99498ef211f4b36aa87918bebcba
junjie-lean <40194410+junjie-lean@users.noreply.github.com>
2019-01-25 15:49:27 +0800

    m

--- config/config.js
+++ config/config.js
@@ -90,7 +90,7 @@ export const G = JSON.parse(data);
 
 window.G = G;
         `;
-        fs.writeFileSync(path.join(process.cwd(), 'src', 'js', 'g.js'), data, { encoding: 'utf8', flag: 'w+' })
+        fs.writeFileSync(path.join('..', 'src', 'js', 'g.js'), data, { encoding: 'utf8', flag: 'w+' })
     }
 }
 
