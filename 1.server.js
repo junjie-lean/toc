3a89e1143dde54d86835ee364ce5b5a10dcf37d3
junjie-lean <40194410+junjie-lean@users.noreply.github.com>
2019-01-28 09:43:59 +0800

    m

---
+++ 1.server.js
@@ -0,0 +1,81 @@
+// import cluster from 'cluster';
+// import os from 'os';
+// import startServer from './toc-server/toc-server';
+// import fs from 'fs';
+// import path from 'path';
+// import config from './config/config';
+// import async from 'async';
+// import chalk from 'chalk';
+// const osType = process.platform;
+// const dev = config.base.isDev;
+// const needClearLog = config.log.needInitCleanLog;
+
+const cluster = require('cluster');
+const os = require('os');
+const fs = require('fs');
+const path = require('path');
+const async = require('async');
+const chalk = require('chalk');
+const config = require('./config/config');
+const startServer = require('./toc-server/toc-server')
+const osType = process.platform;
+const dev = config.base.isDev;
+const needClearLog = config.log.needInitCleanLog;
+
+
+function cleanLog() {
+    if (dev && needClearLog) {
+        let files = fs.readdirSync(path.join(process.cwd(), 'logs'));
+        files.filter(item => {
+            let condition = item.indexOf(".json") == -1;
+            if (condition) {
+                //拿取文件创建时间    
+                let fileCreateTime = fs.statSync(path.join(process.cwd(), 'logs', item)).birthtimeMs;
+                // console.log(
+                //     new Date(fileCreateTime).toLocaleTimeString(),
+                //     new Date().getTime() - fileCreateTime,
+                //     1000 * 60 * 60
+                //     )
+                condition = condition && (new Date().getTime() - fileCreateTime > 1000 * 60 * 60)
+            }
+            return condition;
+        }).map(item => {
+            // console.log(item)
+            console.log(`${chalk.bgRed("  Delete  ")}  ${chalk.red(item)}`);
+            fs.unlinkSync(path.join(process.cwd(), 'logs', item))
+        })
+    }
+
+}
+
+function startCluster() {
+    if (!dev && osType != 'win32' && config.base.isCluster) {
+        if (cluster.isMaster) {
+            for (let i = 0; i < os.cpus().length; i++) {
+                cluster.fork();
+            }
+        } else {
+            startServer()
+        }
+    } else {
+        startServer()
+    }
+}
+
+
+async.series([
+    (callback) => {
+        //g.js写入操作
+        // config.createGlobalFile(config.base);
+        callback();
+    },
+    (callback) => {
+        cleanLog();
+        callback();
+    },
+    (callback) => {
+        startCluster();
+        callback();
+    }
+])
+
